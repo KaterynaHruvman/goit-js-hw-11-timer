d733155b3a1f27dbd097e88be6412702ac0d7a71
Ekateryna Gruvman <kate.gruvman@ukr.net>
2021-03-02 20:44:10 +0200

    timer

--- js/timer.js
+++ js/timer.js
@@ -1,45 +1,61 @@
-// new CountdownTimer({
-//   selector: '#timer-1',
-//   targetDate: new Date('Jul 17, 2019'),
-// });
 
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
 
-const timer = {
-    start() {
-        const startTime = Date.now();
-        setInterval(() => {
-            const currentTime = Date.now();
-            const deltaTime = currentTime - startTime;
-            const { hours, mins, secs } = getTimeComponents(deltaTime);
-            console.log(`${hours}:${mins}:${secs}`);
+  start() {
+    this.timer();
+  }
 
-           
-        }, 1000);
-    },
-};
-timer.start();
+  timer() {
+    const timerDate = this.getTime();
+    const refs = {
+      timerRef: document.querySelector(this.selector),
+      days: document.querySelector('span[data-value="days"]'),
+      hours: document.querySelector('span[data-value="hours"]'),
+      mins: document.querySelector('span[data-value="mins"]'),
+      secs: document.querySelector('span[data-value="secs"]'),
+    };
 
+    refs.days.innerHTML = timerDate.days;
+    refs.hours.innerHTML = timerDate.hours;
+    refs.mins.innerHTML = timerDate.mins;
+    refs.secs.innerHTML = timerDate.secs;
+  }
 
+  getTime() {
+    const targetDate = Date.parse(this.targetDate);
+    const time = targetDate - Date.now();
+    const intervalId = setInterval(this.timer.bind(this), 1000);
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    if (time <= 0) {
+      clearInterval(intervalId);
+    //   intervalId = null;
+    }
+    return {
+      time,
+      days,
+      hours,
+      mins,
+      secs,
+    };
+  }
 
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+}
 
+const newYear = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Dec 31 2021'),
+});
 
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+newYear.start();
